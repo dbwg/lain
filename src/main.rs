8bb181392b29953930481b92adc6233d6c60a4f1
Erin <erin@hashbang.sh>
2017-05-11 11:07:16 -0500

    src/main.rs: Load config and connect to Discord

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate log;
+extern crate serenity;
+extern crate env_logger;
+extern crate toml;
+extern crate dotenv;
+
+mod config;
+
+use std::fs::File;
+use serenity::Client;
+use dotenv::dotenv;
+
+use config::{Configuration, Secrets};
+
 fn main() {
-    println!("Hello, world!");
+	dotenv().ok();
+	env_logger::init().unwrap();
+
+	info!("Loading secrets");
+	let secrets = Secrets::from_env();
+
+	let config_path = "config.toml";
+	info!("Loading config from {}", config_path);
+	let mut config_file = File::open(config_path)
+		.expect("Error opening config file!");
+	let config = Configuration::from_file(&mut config_file);
+
+	let mut client = Client::login_bot(&secrets.token);
+
+	info!("Connecting to Discord with {} shard(s)", config.shards);
+	if let Err(e) = client.start() {
+		error!("Discord client error: {:?}", e);
+	}
 }
