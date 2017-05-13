a9af3581b3f847797902fb2b5a2b4c975afb7520
Erin <erin@hashbang.sh>
2017-05-13 11:59:48 -0500

    www/__init__.py: wire in Flask-SQLAlchemy

--- www/__init__.py
+++ www/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 # -- Set up the Flask application
@@ -7,6 +8,8 @@ app.config.from_object('www.default_config')
 app.config.from_envvar('LAINWWW_CONFIG', silent=True)
 app.jinja_env.auto_reload = app.config['TEMPLATES_AUTO_RELOAD']
 
+# -- Set up Flask extensions
+db = SQLAlchemy(app)
 
 
 # -- Import application structure
