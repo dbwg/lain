c82b658e5b3596d9d3a831759ed3810954eb652c
Erin <erin@hashbang.sh>
2017-05-19 20:42:34 -0500

    Add a commands module, and a command documentation macro

---
+++ src/commands/mod.rs
@@ -0,0 +1,27 @@
+macro_rules! doc_cmd {
+    ($cname:ident, desc => $desc:expr) => {
+        #[allow(non_upper_case_globals)]
+        pub mod $cname {
+            pub static desc: &'static str = $desc;
+        }
+    };
+
+    ($cname:ident, desc => $desc:expr, usage => $usage:expr) => {
+        #[allow(non_upper_case_globals)]
+        pub mod $cname {
+            pub static desc: &'static str = $desc;
+            pub static usage: &'static str = $usage;
+        }
+    };
+
+    ($cname:ident, desc => $desc:expr, usage => $usage:expr, example => $example:expr) => {
+        #[allow(non_upper_case_globals)]
+        pub mod $cname {
+            pub static desc: &'static str = $desc;
+            pub static usage: &'static str = $usage;
+            pub static example: &'static str = $example;
+        }
+    };
+}
+
+pub mod meta;
