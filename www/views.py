5ebaa9f67c651343b93ab32fcf523d9ba1d7a628
Erin <erin@hashbang.sh>
2017-05-31 18:07:36 -0500

    www: add a /dash stub

--- www/views.py
+++ www/views.py
@@ -1,6 +1,13 @@
 from flask import render_template, flash
+from flask_login import login_required
 from www import app
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/dash')
+@login_required
+def dash():
+	return "foooo"
+
