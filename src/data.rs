723ea1582ff68a52da372c03c3256a58d1b9b02b
Erin <erin@hashbang.sh>
2017-05-31 17:55:52 -0500

    Add a helper for getting a Redis connection from ctx

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,6 @@
-use typemap::Key;
-use r2d2::Pool;
+use std::sync::Mutex;
+use typemap::{ShareMap, Key};
+use r2d2::{Pool, PooledConnection};
 use r2d2_redis::RedisConnectionManager;
 
 pub struct RedisPool;
@@ -8,3 +9,15 @@ impl Key for RedisPool {
     type Value = Pool<RedisConnectionManager>;
 }
 
+
+/// Gets a pooled connection to Redis, via a passed reference to `ctx.data`.
+///
+/// # Examples
+///
+/// ```
+/// let conn = ::data;:get_redis_conn(&ctx.data);
+/// ```
+pub fn get_redis_conn(data: &Mutex<ShareMap>) -> PooledConnection<RedisConnectionManager> {
+    let mut data = data.lock().unwrap();
+    data.get_mut::<RedisPool>().unwrap().get().unwrap()
+}
